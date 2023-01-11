b5c84057e31e79d4c7f26ad7e20ee60efa317491
unknown <m_bogdanova621@mail.ru>
2023-01-11 20:08:46 +0400

    animation

--- src/components/Movie/index.tsx
+++ src/components/Movie/index.tsx
@@ -1,20 +1,29 @@
+import classNames from 'classnames';
 import { IMovie } from '../../models';
-import styles from './movie.module.scss';
 import { ReactComponent as New } from '../../images/type=new.svg';
 import { ReactComponent as MoreDetails } from '../../images/moreDetails.svg';
+import TextCard from '../TextCard';
+import styles from './movie.module.scss';
+
 interface IPropsMovie {
   movie: IMovie;
 }
 
-function Movie({
-  movie: { country, genres, length, min_age, num_seasons, title, year, poster, imdb_rate, is_new },
-}: IPropsMovie) {
+function Movie({ movie }: IPropsMovie) {
+  const { poster, imdb_rate, is_new } = movie;
   return (
     <div className={styles.cardMovie}>
       <div className={styles.imgBox}>
-        <img src={`api/${poster}`} alt='picture' />
+        <img src={`api/${poster}`} alt='picture' className={styles.img} />
 
-        <div className={styles.btnOnIMG}>
+        <div className={classNames(styles.btnOnIMG, styles.topBtnOn)}>
+          {is_new && <New className={styles.new} />}
+          <div className={styles.greenButton}>
+            IMDB &nbsp; <span>{imdb_rate}/10</span>
+          </div>
+        </div>
+
+        <div className={classNames(styles.btnOnIMG, styles.bottomBtnOn)}>
           {is_new && <New className={styles.new} />}
           <div className={styles.greenButton}>
             IMDB &nbsp; <span>{imdb_rate}/10</span>
@@ -23,17 +32,7 @@ function Movie({
         <MoreDetails className={styles.moreDatails} />
       </div>
 
-      <div className={styles.text}>
-        <div className={styles.title}>{title}</div>
-        <div className={styles.meta}>
-          <div>{country} </div>
-          <div>{year}</div>
-          <div>{length / 60} min</div>
-          <div>{num_seasons} seasons</div>
-          <div>{min_age}+</div>
-        </div>
-        <div className={styles.genres}>{genres.join(', ')}</div>
-      </div>
+      <TextCard movie={movie} />
     </div>
   );
 }
---
+++ src/components/TextCard/index.tsx
@@ -0,0 +1,24 @@
+import { IMovie } from '../../models';
+import styles from './TextCard.module.scss';
+
+interface IPropsTextCard {
+  movie: IMovie;
+}
+
+function TextCard({ movie: { country, genres, length, min_age, num_seasons, title, year } }: IPropsTextCard) {
+  return (
+    <div className={styles.text}>
+      <div className={styles.title}>{title}</div>
+      <div className={styles.meta}>
+        <div>{country} </div>
+        <div>{year}</div>
+        <div>{length / 60} min</div>
+        <div>{num_seasons} seasons</div>
+        <div>{min_age}+</div>
+      </div>
+      <div className={styles.genres}>{genres.join(', ')}</div>
+    </div>
+  );
+}
+
+export default TextCard;
